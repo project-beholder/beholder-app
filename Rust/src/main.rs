ddabab037788c2d041d2c6fd3f146008dd3b4266
Krithik Ranjan <krithik.ranjan@gmail.com>
2023-10-04 11:24:11 -0600

    idk

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -28,13 +28,13 @@ fn main() {
         marker_detector.observation_loop();
 
         let end_time = now.elapsed().as_millis();
-        // println!("Loop time: {}", end_time-start_time);
+        println!("Loop time: {}", end_time-start_time);
         
         let map = marker_detector.marker_map();
 
         tree.update(map);
 
-
+        
         if now.elapsed().as_secs() > 10 { break; }
     }
 }
